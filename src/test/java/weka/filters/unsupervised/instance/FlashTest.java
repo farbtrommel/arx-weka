80e6436639afd42ae0cfe5172d7924727e1cb3ef
Christian Windolf <christianwindolf@web.de>
2014-11-10 20:11:02 +0100

    test preperations

--- src/test/java/weka/filters/unsupervised/instance/FlashTest.java
+++ src/test/java/weka/filters/unsupervised/instance/FlashTest.java
@@ -1,7 +1,12 @@
 package weka.filters.unsupervised.instance;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -20,6 +25,7 @@ import weka.filters.Filter;
  * @version $Revision: 1.0 $
  */
 public class FlashTest extends AbstractFilterTest {
+	private static final String DATA_PATH = "weka/filters/data/";
 
 	public FlashTest(String name) {
 		super(name);
@@ -27,20 +33,59 @@ public class FlashTest extends AbstractFilterTest {
 
 	@Override
 	public Filter getFilter() {
-		return new Flash();
+		Flash f = new Flash();
+		f.setDataHierarchyFolder(hierarchyFolder);
+		return f;
 	}
+	
+	protected File hierarchyFolder;
 
 	protected void setUp() throws Exception {
+		hierarchyFolder = File.createTempFile("flash", Long.toString(System.currentTimeMillis()));
 		m_Filter = getFilter();
 		m_Instances = new Instances(
 				new BufferedReader(
 						new InputStreamReader(
 								ClassLoader
-										.getSystemResourceAsStream("weka/filters/data/FlashTest.arff"))));
+										.getSystemResourceAsStream(DATA_PATH + "FlashTest.arff"))));
 		m_Instances.setClassIndex(1);
 		m_OptionTester = getOptionTester();
 		m_GOETester = getGOETester();
 		m_FilteredClassifier = getFilteredClassifier();
+		
+		
+		
+		if(hierarchyFolder.exists()){
+			throw new IOException("I need an empty folder " + hierarchyFolder.getAbsolutePath() + " but it already exists");
+		}
+		if(!hierarchyFolder.mkdir()){
+			throw new IOException("I need to be able to create the folder " + hierarchyFolder.getAbsolutePath() + " but I can't (check write permission?)");
+		}
+		copyHierarchies("age");
+		copyHierarchies("car");
+		copyHierarchies("children");
+		copyHierarchies("name");
+		copyHierarchies("zip");
+		
+		
+		
+	}
+	
+	private void copyHierarchies(String hierarchy) throws IOException{
+		String filename = "test_hierachry_" + hierarchy + ".csv";
+		InputStream in = ClassLoader.getSystemResourceAsStream(DATA_PATH + filename);
+		File file = new File(hierarchyFolder, filename);
+		file.createNewFile();
+		OutputStream out = new FileOutputStream(file);
+		byte[] buffer = new byte[1024];
+		int len = in.read(buffer);
+		while(len != -1) {
+			out.write(buffer, 0, len);
+			len = in.read(buffer);
+		}
+		in.close();
+		out.close();
+		
 	}
 
 	protected void tearDown() {
