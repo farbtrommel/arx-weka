c7b61f57f6f38a5ffb1fce94258908b96e061578
Christian Windolf <christianwindolf@web.de>
2015-04-05 13:22:31 +0200

    add ldiversity test

--- src/test/java/weka/filters/unsupervised/instance/FlashTest.java
+++ src/test/java/weka/filters/unsupervised/instance/FlashTest.java
@@ -9,15 +9,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.filters.AbstractFilterTest;
 import weka.filters.Filter;
+import weka.test.Regression;
 
 /**
  * Tests Flash. Run from the command line with:
@@ -40,6 +46,8 @@ public class FlashTest extends AbstractFilterTest {
 		Flash f = new Flash();
 		f.setDataHierarchyFolder(hierarchyFolder);
 		f.setDataSensitiveAttributes("6,7");
+		f.setEnableLDiversity(true);
+		f.setValueL(2);
 		return f;
 	}
 	
@@ -108,6 +116,7 @@ public class FlashTest extends AbstractFilterTest {
 		m_OptionTester = null;
 		m_GOETester = null;
 		m_FilteredClassifier = null;
+		super.tearDown();
 	}
 	
 
@@ -124,40 +133,74 @@ public class FlashTest extends AbstractFilterTest {
 		assertTrue("The result has an identifier that occurs less than 2 times", checkKAnonymity(result, 2,0,1,2,3,4));
 	}
 	
+	public void testLDiversity(){
+		Instances result = useFilter();
+		assertTrue("The result has less than l different sensitive values of quasi-identifiying class", checkLDiversity(result, 2, 0,1,2,3,4));
+	}
+	
+	
 	private boolean checkKAnonymity(Instances instances, int k, int ...identifiers ){
-		Map<String, Integer> counters = new HashMap<String, Integer>();
+		Map<String, List<String>> map = identifieres2Sensitive(instances, identifiers);
 		
-		for(int i = 0; i < instances.numInstances(); i++){
-			StringBuilder identifier = new StringBuilder();
-			for(int id : identifiers){
-				Instance instance = instances.instance(i);
-				Attribute attr = instance.attribute(id);
-				if(attr.type() == NUMERIC){
-					identifier.append(instance.value(id));
-				} else {
-					identifier.append(instance.stringValue(id));
-				}
-				
-			}
-			Integer counter = counters.get(identifier.toString());
-			if(counter == null){
-				counter = new Integer(1);
-			} else {
-				counter = new Integer(counter + 1);
-			}
-			counters.put(identifier.toString(), counter);
-		}
-		for(Entry<String, Integer> entry : counters.entrySet()){
-			if(entry.getValue() < k){
+		for(Entry<String, List<String>> entry : map.entrySet()){
+			if(entry.getValue().size() < k){
 				return false;
 			}
 		}
 		
 		return true;
+	}
+	
+	private boolean checkLDiversity(Instances instances, int l, int ...identifiers){
+		Map<String, List<String>> map = identifieres2Sensitive(instances, identifiers);
+		
+		for(Entry<String, List<String>> entry : map.entrySet()){
+			Set<String> set = new HashSet<String>();
+			for(String sensitive : entry.getValue()){
+				set.add(sensitive);
+			}
+			if(set.size() < l){
+				return false;
+			}
+			
+		}
+		return true;
 		
 	}
 	
+	private Map<String, List<String>> identifieres2Sensitive(Instances instances, int ... identifiers){
+		Map<String, List<String>> result = new HashMap<String, List<String>>();
+		for(int i = 0; i < instances.numInstances(); i++){
+			StringBuilder identifier = new StringBuilder();
+			Instance instance = instances.instance(i);
+			for(int id : identifiers){
+				Attribute attr = instance.attribute(id);
+				identifier.append(attribute2String(instance, attr));
+			}
+			
+			StringBuilder sensitive = new StringBuilder();
+			for(int j = 0; j < instances.numAttributes(); j++){
+				if(!Arrays.asList(identifiers).contains(j)){
+					sensitive.append(attribute2String(instance, instances.attribute(j)));
+				}
+			}
+			List<String> list = result.get(identifier.toString());
+			if(list == null){
+				list = new ArrayList<String>(3);
+			}
+			list.add(sensitive.toString());
+			result.put(identifier.toString(), list);
+		}
+		return result;
+	}
 	
+	private String attribute2String(Instance instance, Attribute attr){
+		if(attr.type() == NUMERIC){
+			return String.valueOf(instance.value(attr));
+		} else {
+			return instance.stringValue(attr);
+		}
+	}
 	
 	
 
